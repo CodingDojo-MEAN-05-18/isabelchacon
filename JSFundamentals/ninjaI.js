2e5ac8afd207370d77649b0261f93e3898f586ed
isabelchacon <isabel@cogwheelclinic.com>
2018-05-13 20:11:12 -0700

    Ninja II

--- JSFundamentals/ninjaI.js
+++ JSFundamentals/ninjaI.js
@@ -8,12 +8,18 @@ function Ninja(name) {
 
     this.showStats = function() {
         console.log("Name: "+this.name+", Health: "+this.health+", Speed: "+speed+", Strength: "+strength);
+        return this;    
     };
+
+    this.getstrength = function(){
+        return strength;
+    }
 
 }
 
 Ninja.prototype.sayName = function() {
     console.log("My name is "+this.name);
+    return this;
 }
 
 Ninja.prototype.drinkSake = function() {
@@ -21,7 +27,34 @@ Ninja.prototype.drinkSake = function() {
     return this;
 }
 
-var ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.drinkSake();
-ninja1.showStats();
+Ninja.prototype.punch= function(ninja2) {
+    if(!(this instanceof Ninja) || !(ninja2 instanceof Ninja)){
+        console.log("Ninjas can only punch other ninjas.")
+    }
+    else{
+        ninja2.health -= 5;
+        console.log(ninja2.name+" was punched by "+this.name+" and lost 5 health");
+    }
+    return this;
+}
+
+Ninja.prototype.kick= function(ninja2) {
+    if(!(this instanceof Ninja) || !(ninja2 instanceof Ninja)){
+        console.log("Ninjas can only kick other ninjas.")
+    }
+    else{
+        var health = this.getstrength() * 15
+        ninja2.health -= health;
+        console.log(ninja2.name+" was kicked by "+this.name+" and lost "+health+" health!");
+    }
+    return this;
+}
+
+
+var ana = new Ninja("Ana");
+var isabel = new Ninja("Isabel");
+
+var num = 5;
+
+ana.kick(isabel);
+isabel.showStats();
